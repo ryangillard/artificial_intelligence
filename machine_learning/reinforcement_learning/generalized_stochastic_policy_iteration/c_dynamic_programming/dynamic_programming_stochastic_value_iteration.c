8a2846054ebf662072b1c61dae62df0f057d415e
Ryan Gillard <34553410+ryangillard@users.noreply.github.com>
2018-11-08 16:16:19 -0800

    fixed indented block

--- machine_learning/reinforcement_learning/generalized_stochastic_policy_iteration/c_dynamic_programming/dynamic_programming_stochastic_value_iteration.c
+++ machine_learning/reinforcement_learning/generalized_stochastic_policy_iteration/c_dynamic_programming/dynamic_programming_stochastic_value_iteration.c
@@ -430,14 +430,14 @@ void GreedyPolicySelection(unsigned int number_of_non_terminal_states, unsigned 
 		/* Apportion policy probability across ties equally for state-action pairs that have the same value and zero otherwise */
 		for (j = 0; j < number_of_actions_per_non_terminal_state[i]; j++)
 		{
-				if (policy[i][j] == max_policy_value)
-				{
-					policy[i][j] = 1.0 / max_policy_count;
-				}
-				else
-				{
-					policy[i][j] = 0.0;
-				}
+			if (policy[i][j] == max_policy_value)
+			{
+				policy[i][j] = 1.0 / max_policy_count;
+			}
+			else
+			{
+				policy[i][j] = 0.0;
+			}
 		} // end of j loop
 	} // end of i loop
 } // end of GreedyPolicySelection function
