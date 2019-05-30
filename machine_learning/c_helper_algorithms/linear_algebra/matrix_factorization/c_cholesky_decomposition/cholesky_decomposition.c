c5e9cee42e188c506ffa7fff7f0a5370715fbcc0
Ryan Gillard <ryangillard@google.com>
2019-05-30 09:31:31 +0000

    added c cholesky decomposition

---
+++ machine_learning/c_helper_algorithms/linear_algebra/matrix_factorization/c_cholesky_decomposition/cholesky_decomposition.c
@@ -0,0 +1,306 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+/*********************************************************************************************************/
+/********************************************** PROTOTYPES ***********************************************/
+/*********************************************************************************************************/
+
+/* This function computes the Cholesky factorization of a real symmetric positive definite matrix A using the recursive algorithm. */
+int recursive_cholesky_factorization(int n, double** a, int a_row_offset, int a_col_offset);
+
+/* This function solves the matrix equation X * A**T = B for triangular matrix A */
+int solve_triangular_matrix_equation(int m, int n, double** a, int a_row_offset, int a_col_offset, int b_row_offset, int b_col_offset);
+
+/* This function performs the symmetric rank k operation C := -A * A**T + C */
+int symmetric_rank_k_operation(int n, int k, double** a, int a_row_offset, int a_col_offset, int c_row_offset, int c_col_offset);
+
+/*********************************************************************************************************/
+/************************************************* MAIN **************************************************/
+/*********************************************************************************************************/
+
+int main(int argc, char* argv[])
+{
+	int i, j, system_return = 0, error = 0;
+	
+	/* Read inputs */
+	/* Get the order of symmetric matrix */
+	int order = 0;
+	
+	FILE* infile_order = fopen("inputs/order.txt", "r");
+	system_return = fscanf(infile_order, "%u", &order);
+	if (system_return == -1)
+	{
+		printf("Failed reading file inputs/order.txt\n");
+	}
+	fclose(infile_order);
+	printf("order = %d\n", order);
+	
+	/* Now get the matrix values */
+	double** matrix;
+	
+	printf("matrix = \n");
+	FILE* infile_matrix = fopen("inputs/matrix.txt", "r");
+	matrix = malloc(sizeof(double*) * order);
+	for (i = 0; i < order; i++)
+	{
+		matrix[i] = malloc(sizeof(double) * order);
+		for (j = 0; j < order; j++)
+		{
+			system_return = fscanf(infile_matrix, "%lf\t", &matrix[i][j]);
+			if (system_return == -1)
+			{
+				printf("Failed reading file inputs/matrix.txt\n");
+			}
+			else
+			{
+				printf("%lf\t", matrix[i][j]);
+			}
+		} // end of j loop
+		printf("\n");
+	} // end of i loop		
+	fclose(infile_matrix);
+	
+	/* Call function to perform Cholesky decomposition on our real symmetric positive definite matrix */
+	error = recursive_cholesky_factorization(order, matrix, 0, 0);
+	
+	/* Print L or error code message */
+	if (error == 0)
+	{
+		/* Zero upper triangular matrix without diagonal since it wasn't updated */
+		for (i = 0; i < order - 1; i++)
+		{
+			for (j = i + 1; j < order; j++)
+			{
+				matrix[i][j] = 0.0;
+			} // end of j loop
+		} // end of i loop
+
+		printf("L = \n");
+		for (i = 0; i < order; i++)
+		{
+			for (j = 0; j < order; j++)
+			{
+				printf("%lf\t", matrix[i][j]);
+			} // end of j loop
+			printf("\n");
+		} // end of i loop
+	}
+	else if (error == 1)
+	{
+		printf("ERROR: recursive_cholesky_factorization somehow managed to start with n == 0\n");
+	}
+	else if (error == 2)
+	{
+		printf("ERROR: solve_triangular_matrix_equation somehow managed to start with m or n == 0\n");
+	}
+	else if (error == 3)
+	{
+		printf("ERROR: symmetric_rank_k_operation somehow managed to start with n or k == 0\n");
+	}
+	else
+	{
+		printf("ERROR: Matrix is not positive-definite!\n");
+	}
+	
+	/* Free dynamically allocated memory */
+	for (i = 0; i < order; i++)
+	{
+		free(matrix[i]);
+	} // end of i loop
+	free(matrix);
+	
+	return 0;
+} // end of main
+
+/*********************************************************************************************************/
+/*********************************************** FUNCTIONS ***********************************************/
+/*********************************************************************************************************/
+
+/*
+This function computes the Cholesky factorization of a real symmetric
+positive definite matrix A using the recursive algorithm.
+
+The factorization has the form
+   A = L  * L**T, 
+where L is lower triangular.
+
+This is the recursive version of the algorithm. It divides
+the matrix into four submatrices:
+
+       [  A11 | A12  ]  where A11 is n1 by n1 and A22 is n2 by n2
+   A = [ -----|----- ]  with n1 = n / 2 and n2 = n - n1
+       [  A21 | A22  ]  
+
+The function calls itself to factor A11. Update and scale A21, 
+update A22, then calls itself to factor A22.
+Modified from dpotrf2.
+*/
+int recursive_cholesky_factorization(int n, double** a, int a_row_offset, int a_col_offset)
+{
+	/*
+	Args:
+		n: The order of the matrix a. n >= 0.
+		a: Matrix that we are modifying in place.
+		a_row_offset: Number of rows we need to offset a.
+		a_col_offset: Number of cols we need to offset a.
+		
+	Returns:
+		Integer error code.
+	*/
+	
+	int i, j, error = 0;
+	
+	/* Quick return if possible */
+	if (n == 0)
+	{
+		return 1;
+	}
+
+	if (n == 1) // n == 1 case
+	{
+		/* Test for non-positive-definiteness */
+		if (a[a_row_offset][a_col_offset] <= 0.0 || isnan(a[a_row_offset][a_col_offset]))
+		{
+			return 99;
+		}
+
+		/* Factor */
+		a[a_row_offset][a_col_offset] = sqrt(a[a_row_offset][a_col_offset]);
+	}
+	else // use recursive code
+	{
+		int n1 = n / 2;
+		int n2 = n - n1;
+
+		/* Factor A11 */
+		error = recursive_cholesky_factorization(n1, a, a_row_offset, a_col_offset);
+		if (error != 0)
+		{
+			return error;
+		}		
+
+		/* Update and scale A21 */
+		error = solve_triangular_matrix_equation(n2, n1, a, a_row_offset, a_col_offset, n1 + a_row_offset, a_col_offset);
+		if (error != 0)
+		{
+			return error;
+		}
+
+		/* Update A22 */
+		error = symmetric_rank_k_operation(n2, n1, a, n1 + a_row_offset, a_col_offset, n1 + a_row_offset, n1 + a_col_offset);
+		if (error != 0)
+		{
+			return error;
+		}
+		
+		/* Factor A22 */
+		error = recursive_cholesky_factorization(n2, a, n1 + a_row_offset, n1 + a_col_offset);
+	}
+	
+	return error;
+} // end of recursive_cholesky_factorization function
+
+/*
+This function solves the matrix equation X * A**T = B,
+where X and B are m by n matrices, A is a non-unit, 
+lower triangular matrix.
+Modified from dtrsm.
+*/
+int solve_triangular_matrix_equation(int m, int n, double** a, int read_row_offset, int read_col_offset, int write_row_offset, int write_col_offset)
+{
+	/*
+	Args:
+		m: Number of rows of current submatrix of a.
+		n: Number of cols of current submatrix of a.
+		a: Matrix that we are modifying in place.
+		read_row_offset: Number of rows we need to offset for reading from a.
+		read_col_offset: Number of cols we need to offset for reading from a.
+		write_row_offset: Number of rows we need to offset for writing to a.
+		write_col_offset: Number of cols we need to offset for writing to a.
+		
+	Returns:
+		Integer error code.
+	*/
+	
+	int i, j, k;
+	double temp;
+	
+	/* Quick return if possible. */
+	if (m == 0 || n == 0)
+	{
+		return 2;
+	}
+	/* Form  B := B * inv( A**T ). */
+	for (k = 0; k < n; k++)
+	{
+		temp = 1.0 / a[k + read_row_offset][k + read_col_offset];
+		for (i = 0; i < m; i++)
+		{
+			a[i + write_row_offset][k + write_col_offset] *= temp;
+		} // end of i loop
+		
+		for (j = k + 1; j < n; j++)
+		{
+			if (a[j + read_row_offset][k + read_col_offset] != 0.0)
+			{
+				temp = a[j + read_row_offset][k + read_col_offset];
+				for (i = 0; i < m; i++)
+				{
+					a[i + write_row_offset][j + write_col_offset] -= temp * a[i + write_row_offset][k + write_col_offset];
+				} // end of i loop
+			}
+		} // end of j loop
+	} // end of k loop
+	
+	return 0;				
+} // end of solve_triangular_matrix_equation function
+
+/*
+This function performs the symmetric rank k operation C := -A * A**T + C,
+where C is an n by n symmetric matrix and A is an n by k matrix.
+Modified from dsyrk.
+*/
+int symmetric_rank_k_operation(int n, int k, double** a, int read_row_offset, int read_col_offset, int write_row_offset, int write_col_offset)
+{
+	/*
+	Args:
+		n: Number of rows of current submatrix of a.
+		k: Number of cols of current submatrix of a.
+		a: Matrix that we are modifying in place.
+		read_row_offset: Number of rows we need to offset for reading from a.
+		read_col_offset: Number of cols we need to offset for reading from a.
+		write_row_offset: Number of rows we need to offset for writing to a.
+		write_col_offset: Number of cols we need to offset for writing to a.
+		
+	Returns:
+		Integer error code.
+	*/
+	
+	int i, j, l;
+	double temp;
+	
+	/* Quick return if possible. */
+	if (n == 0 || k == 0)
+	{
+		return 3;
+	}
+	
+	/* Form  C := -A*A**T + C. */
+	for (j = 0; j < n; j++)
+	{
+		for (l = 0; l < k; l++)
+		{
+			if (a[j + read_row_offset][l + read_col_offset] != 0.0)
+			{
+				temp = -a[j + read_row_offset][l + read_col_offset];
+				for (i = j - 1; i < n; i++)
+				{
+					a[i + write_row_offset][j + write_col_offset] += temp * a[i + read_row_offset][l + read_col_offset];
+				} // end of i loop
+			}
+		} // end of l loop
+	} // end of j loop
+
+	return 0;
+} // end of symmetric_rank_k_operation function
