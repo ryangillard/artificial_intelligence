e1497d7338ddf5f6255eb4afbc596032ca19e7ce
Ryan Gillard <ryangillard@google.com>
2019-06-06 11:29:41 +0000

    added recombination

--- machine_learning/bayesian_learning/gaussian_process/gaussian_process_regression/c_gaussian_process_regression/gaussian_process_regression.c
+++ machine_learning/bayesian_learning/gaussian_process/gaussian_process_regression/c_gaussian_process_regression/gaussian_process_regression.c
@@ -62,6 +62,9 @@ Modified from dscal.
 */
 void dscal(unsigned int n, double da, double** a, unsigned int row_offset, unsigned int col_offset);
 
+/* This function efficiently recombines a lower cholesky decomposition inverse A^-1 = L^-1 * L^-T */
+void RecombineLowerCholeskyDecompositionInverse(unsigned int n, double** L, double** A);
+
 /* This function uses the iterative conjugate gradient method that solves A * x = b */
 void ConjugateGradientMethod(unsigned int n, double** A, double** b, double** x);
 
@@ -1177,201 +1180,221 @@ void dscal(unsigned int n, double da, double** a, unsigned int row_offset, unsig
 	return;
 } // end of dscal function
 
-// void OptimizeKernelHyperparameters(int kernel_type, unsigned int num_training_points, unsigned int num_dimensions, double** X_train, double** kernel_x_x, double length_scale, double signal_variance, double noise_variance)
-// {
-// 	unsigned int i, j;
+/* This function efficiently recombines a lower cholesky decomposition inverse A^-1 = L^-1 * L^-T */
+void RecombineLowerCholeskyDecompositionInverse(unsigned int n, double** L, double** A)
+{
+	unsigned int i, j, k;
 	
-// 	if (kernel_type == 0) // linear
-// 	{
-// 		return; // maybe tune constant c???
-// 	}
-// 	else // squared exponential
-// 	{
-// 		/* Length-scale */
-// 		double** d_kernel_d_length_scale_temp;
-// 		d_kernel_d_length_scale_temp = malloc(sizeof(double*) * num_training_points);
-// 		for (i = 0; i < num_training_points; i++)
-// 		{
-// 			d_kernel_d_length_scale_temp[i] = malloc(sizeof(double) * num_training_points);
-// 			for (j = 0; j < num_training_points; j++)
-// 			{
-// 				d_kernel_d_length_scale_temp[i][j] = 0.0;
-// 			} // end of j loop
-// 		} // end of i loop
-		
-// 		double a_squared_sum, b_squared_sum;
-		
-// 		MatrixMultiplication(num_training_points, num_training_points, num_dimensions, X_train, X_train, 0, 1, d_kernel_d_length_scale_temp);
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < i + 1; j++)
+		{
+			A[i][j] = 0.0;
+			for (k = 0; k < j + 1; k++)
+			{
+				A[i][j] += L[i][k] * L[j][k];
+			} // end of k loop
+		} // end of j loop
+	} // end of i loop
+	
+	return
+} // end of RecombineCholeskyDecompositionInverse function
 
-// 		for (i = 0; i < num_training_points; i++)
-// 		{
-// 			for (j = 0; j < num_training_points; j++)
-// 			{
-// 				a_squared_sum = 0.0;
-// 				b_squared_sum = 0.0;
-// 				for (k = 0; k < num_dimensions; k++)
-// 				{
-// 					a_squared_sum += A[i][k] * A[i][k];
-// 					b_squared_sum += B[j][k] * B[j][k];
-// 				} // end of k loop
+void OptimizeKernelHyperparameters(int kernel_type, unsigned int num_training_points, unsigned int num_dimensions, double** X_train, double** kernel_x_x, double length_scale, double signal_variance, double noise_variance)
+{
+	unsigned int i, j;
+	
+	if (kernel_type == 0) // linear
+	{
+		return; // maybe tune constant c???
+	}
+	else // squared exponential
+	{
+		/* Length-scale */
+		double** d_kernel_d_length_scale_temp;
+		d_kernel_d_length_scale_temp = malloc(sizeof(double*) * num_training_points);
+		for (i = 0; i < num_training_points; i++)
+		{
+			d_kernel_d_length_scale_temp[i] = malloc(sizeof(double) * num_training_points);
+			for (j = 0; j < num_training_points; j++)
+			{
+				d_kernel_d_length_scale_temp[i][j] = 0.0;
+			} // end of j loop
+		} // end of i loop
+		
+		double a_squared_sum, b_squared_sum;
+		
+		MatrixMultiplication(num_training_points, num_training_points, num_dimensions, X_train, X_train, 0, 1, d_kernel_d_length_scale_temp);
 
-// 				kernel[i][j] = a_squared_sum + b_squared_sum - 2.0 * kernel[i][j];
-// 			} // end of j loop
-// 		} // end of i loop
+		for (i = 0; i < num_training_points; i++)
+		{
+			for (j = 0; j < num_training_points; j++)
+			{
+				a_squared_sum = 0.0;
+				b_squared_sum = 0.0;
+				for (k = 0; k < num_dimensions; k++)
+				{
+					a_squared_sum += A[i][k] * A[i][k];
+					b_squared_sum += B[j][k] * B[j][k];
+				} // end of k loop
+
+				kernel[i][j] = a_squared_sum + b_squared_sum - 2.0 * kernel[i][j];
+			} // end of j loop
+		} // end of i loop
 		
-// 		double** d_kernel_d_length_scale;
-// 		d_kernel_d_length_scale = malloc(sizeof(double*) * num_training_points);
-// 		for (i = 0; i < num_training_points; i++)
-// 		{
-// 			d_kernel_d_length_scale[i] = malloc(sizeof(double) * num_training_points);
-// 			for (j = 0; j < num_training_points; j++)
-// 			{
-// 				d_kernel_d_length_scale[i][j] = 0.0;
-// 			} // end of j loop
-// 		} // end of i loop
+		double** d_kernel_d_length_scale;
+		d_kernel_d_length_scale = malloc(sizeof(double*) * num_training_points);
+		for (i = 0; i < num_training_points; i++)
+		{
+			d_kernel_d_length_scale[i] = malloc(sizeof(double) * num_training_points);
+			for (j = 0; j < num_training_points; j++)
+			{
+				d_kernel_d_length_scale[i][j] = 0.0;
+			} // end of j loop
+		} // end of i loop
 		
-// 		MatrixMultiplication(num_training_points, num_training_points, num_training_points, kernel_x_x, d_kernel_d_length_scale_temp, 0, 0, d_kernel_d_length_scale);
+		MatrixMultiplication(num_training_points, num_training_points, num_training_points, kernel_x_x, d_kernel_d_length_scale_temp, 0, 0, d_kernel_d_length_scale);
 		
-// 		for (i = 0; i < num_training_points; i++)
-// 		{
-// 			for (j = 0; j < num_training_points; j++)
-// 			{
-// 				d_kernel_d_length_scale[i][j] /= pow(length_scale, 3);
-// 			} // end of j loop
-// 		} // end of i loop
+		for (i = 0; i < num_training_points; i++)
+		{
+			for (j = 0; j < num_training_points; j++)
+			{
+				d_kernel_d_length_scale[i][j] /= pow(length_scale, 3);
+			} // end of j loop
+		} // end of i loop
 			
-// 		/* Signal variance */
-// 		double** d_kernel_d_signal_variance;
-// 		d_kernel_d_signal_variance = malloc(sizeof(double*) * num_training_points);
-// 		for (i = 0; i < num_training_points; i++)
-// 		{
-// 			d_kernel_d_signal_variance[i] = malloc(sizeof(double) * num_training_points);
-// 			for (j = 0; j < num_training_points; j++)
-// 			{
-// 				if (i == j)
-// 				{
-// 					d_kernel_d_signal_variance[i][j] = 2.0 * (kernel_x_x[i][j] - noise_variance * noise_variance) / signal_variance;
-// 				}
-// 				else
-// 				{
-// 					d_kernel_d_signal_variance[i][j] = 2.0 * kernel_x_x[i][j] / signal_variance;
-// 				}
-// 			} // end of j loop
-// 		} // end of i loop
+		/* Signal variance */
+		double** d_kernel_d_signal_variance;
+		d_kernel_d_signal_variance = malloc(sizeof(double*) * num_training_points);
+		for (i = 0; i < num_training_points; i++)
+		{
+			d_kernel_d_signal_variance[i] = malloc(sizeof(double) * num_training_points);
+			for (j = 0; j < num_training_points; j++)
+			{
+				if (i == j)
+				{
+					d_kernel_d_signal_variance[i][j] = 2.0 * (kernel_x_x[i][j] - noise_variance * noise_variance) / signal_variance;
+				}
+				else
+				{
+					d_kernel_d_signal_variance[i][j] = 2.0 * kernel_x_x[i][j] / signal_variance;
+				}
+			} // end of j loop
+		} // end of i loop
 		
-// 		/* Noise variance */
-// 		double** d_kernel_d_noise_variance;
-// 		d_kernel_d_noise_variance = malloc(sizeof(double*) * num_training_points);
-// 		for (i = 0; i < num_training_points; i++)
-// 		{
-// 			d_kernel_d_noise_variance[i] = malloc(sizeof(double) * num_training_points);
-// 			for (j = 0; j < num_training_points; j++)
-// 			{
-// 				if (i == j)
-// 				{
-// 					d_kernel_d_noise_variance[i][j] = 2.0 * noise_variance;
-// 				}
-// 				else
-// 				{
-// 					d_kernel_d_noise_variance[i][j] = 0.0;
-// 				}
-// 			} // end of j loop
-// 		} // end of i loop
+		/* Noise variance */
+		double** d_kernel_d_noise_variance;
+		d_kernel_d_noise_variance = malloc(sizeof(double*) * num_training_points);
+		for (i = 0; i < num_training_points; i++)
+		{
+			d_kernel_d_noise_variance[i] = malloc(sizeof(double) * num_training_points);
+			for (j = 0; j < num_training_points; j++)
+			{
+				if (i == j)
+				{
+					d_kernel_d_noise_variance[i][j] = 2.0 * noise_variance;
+				}
+				else
+				{
+					d_kernel_d_noise_variance[i][j] = 0.0;
+				}
+			} // end of j loop
+		} // end of i loop
 		
-// 		/* Free dynamic memory */
-// 		for (i = 0; i < num_training_points; i++)
-// 		{
-// 			free(d_kernel_d_noise_variance[i]);
-// 			free(d_kernel_d_signal_variance[i]);
-// 			free(d_kernel_d_length_scale[i]);
-// 		} // end of i loop
-// 		free(d_kernel_d_noise_variance);
-// 		free(d_kernel_d_signal_variance);
-// 		free(d_kernel_d_length_scale);
-// 	}
+		/* Free dynamic memory */
+		for (i = 0; i < num_training_points; i++)
+		{
+			free(d_kernel_d_noise_variance[i]);
+			free(d_kernel_d_signal_variance[i]);
+			free(d_kernel_d_length_scale[i]);
+		} // end of i loop
+		free(d_kernel_d_noise_variance);
+		free(d_kernel_d_signal_variance);
+		free(d_kernel_d_length_scale);
+	}
 	
-// 	return;
-// } // end of OptimizeKernelHyperparameters function
+	return;
+} // end of OptimizeKernelHyperparameters function
 
-// /* This function uses the iterative conjugate gradient method that solves A * x = b */
-// void ConjugateGradientMethod(unsigned int n, double** A, double** b, double** x)
-// {
-// 	/* A = [n, n] */
-// 	/* b = [n, 1] */
-// 	/* x = [n, 1] */
+/* This function uses the iterative conjugate gradient method that solves A * x = b */
+void ConjugateGradientMethod(unsigned int n, double** A, double** b, double** x)
+{
+	/* A = [n, n] */
+	/* b = [n, 1] */
+	/* x = [n, 1] */
 	
-// 	unsigned int i, j;
+	unsigned int i, j;
 	
-// 	double** r;
-// 	r = malloc(sizeof(double*) * n);
-// 	for (i = 0; i < n; i++)
-// 	{
-// 		r[i] = malloc(sizeof(double) * 1);
-// 	} // end of i loop
+	double** r;
+	r = malloc(sizeof(double*) * n);
+	for (i = 0; i < n; i++)
+	{
+		r[i] = malloc(sizeof(double) * 1);
+	} // end of i loop
 	
-// 	MatrixMultiplication(n, 1, n, A, x, 0, 0, r);
+	MatrixMultiplication(n, 1, n, A, x, 0, 0, r);
 	
-// 	for (i = 0; i < n; i++)
-// 	{
-// 		r[i][0] = b[i][0] - r[i][0];
-// 	} // end of i loop
+	for (i = 0; i < n; i++)
+	{
+		r[i][0] = b[i][0] - r[i][0];
+	} // end of i loop
 	
-// 	double** p;
-// 	p = malloc(sizeof(double*) * n);
-// 	for (i = 0; i < n; i++)
-// 	{
-// 		p[i] = malloc(sizeof(double) * 1);
-// 	} // end of i loop
+	double** p;
+	p = malloc(sizeof(double*) * n);
+	for (i = 0; i < n; i++)
+	{
+		p[i] = malloc(sizeof(double) * 1);
+	} // end of i loop
 	
-// 	double rsold = 0.0, rsnew = 0;
-// 	rsold = VectorDotProductRank2(n, r, r, 1, 1);
+	double rsold = 0.0, rsnew = 0;
+	rsold = VectorDotProductRank2(n, r, r, 1, 1);
 	
-// 	double** Ap;
-// 	Ap = malloc(sizeof(double*) * n);
-// 	for (i = 0; i < n; i++)
-// 	{
-// 		Ap[i] = malloc(sizeof(double) * 1);
-// 	} // end of i loop
+	double** Ap;
+	Ap = malloc(sizeof(double*) * n);
+	for (i = 0; i < n; i++)
+	{
+		Ap[i] = malloc(sizeof(double) * 1);
+	} // end of i loop
 	
-// 	for (i = 0; i < n; i++)
-// 	{
-// 		MatrixMultiplication(n, 1, n, A, p, 0, 0, Ap);
+	for (i = 0; i < n; i++)
+	{
+		MatrixMultiplication(n, 1, n, A, p, 0, 0, Ap);
 		
-// 		alpha = rsold / VectorDotProductRank2(n, p, Ap, 1, 1);
+		alpha = rsold / VectorDotProductRank2(n, p, Ap, 1, 1);
 		
-// 		for (j = 0; j < n; j++)
-// 		{
-// 			x[j][0] += alpha * p[j][0];
-// 			r[j][0] -= alpha * Ap[j][0];
-// 		} // end of j loop
+		for (j = 0; j < n; j++)
+		{
+			x[j][0] += alpha * p[j][0];
+			r[j][0] -= alpha * Ap[j][0];
+		} // end of j loop
 		
-// 		rsnew = VectorDotProductRank2(n, r, r, 1, 1);
-// 		if (sqrt(rsnew) < 1e-10)
-// 		{
-// 			break;
-// 		}
+		rsnew = VectorDotProductRank2(n, r, r, 1, 1);
+		if (sqrt(rsnew) < 1e-10)
+		{
+			break;
+		}
 
-// 		for (j = 0; j < n; j++)
-// 		{
-// 			p[j][0] = r[j][0] + (rsnew / rsold) * p[j][0];
-// 		} // end of j loop
+		for (j = 0; j < n; j++)
+		{
+			p[j][0] = r[j][0] + (rsnew / rsold) * p[j][0];
+		} // end of j loop
 		
-//         rsold = rsnew;
-// 	} // end of i loop
+        rsold = rsnew;
+	} // end of i loop
 	
-// 	/* Free dynamic memory */
-// 	for (i = 0; i < n; i++)
-// 	{
-// 		free(Ap[i]);
-// 		free(p[i]);
-// 		free(r[i]);
-// 	} // end of i loop
-// 	free(Ap);
-// 	free(p);
-// 	free(r);
+	/* Free dynamic memory */
+	for (i = 0; i < n; i++)
+	{
+		free(Ap[i]);
+		free(p[i]);
+		free(r[i]);
+	} // end of i loop
+	free(Ap);
+	free(p);
+	free(r);
 
-// 	return;
-// } // end of ConjugateGradientMethod function
+	return;
+} // end of ConjugateGradientMethod function
 
 /* This function performs matrix multiplication between two given matrices */
 void MatrixMultiplication(unsigned int m, unsigned int n, unsigned int p, double** A, double** B, int transpose_A, int transpose_B, double** C)
